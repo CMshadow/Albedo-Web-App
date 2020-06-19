c93a85910e34efc8f3e2a95626819afd3227e03e
CMshadow <bjwmc1996@gmail.com>
2020-06-18 19:03:29 -0700

    fix getInitial bug

--- src/utils/genInitial.js
+++ src/utils/genInitial.js
@@ -1,11 +1,12 @@
 export const genInitial = (cognitoUser) => {
+  console.log(Object.keys(cognitoUser.attributes))
   if (cognitoUser) {
     const locale = cognitoUser.attributes.locale;
     switch (locale) {
       case 'zh-CN':
         return cognitoUser.attributes.given_name;
       default:
-        return cognitoUser.attributes.given_name[0] + cognitoUser.attributes.last_name[0]
+        return cognitoUser.attributes.given_name[0] + cognitoUser.attributes.family_name[0]
     }
   }
 }
