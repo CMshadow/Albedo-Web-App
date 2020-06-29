c5ebd239512a48c30148222624b0867d4a4de97c
CMshadow <bjwmc1996@gmail.com>
2020-06-28 17:59:01 -0700

    fix konva flow

--- src/pages/SingleLineDiagram/SingleLineDiagram_us.js
+++ src/pages/SingleLineDiagram/SingleLineDiagram_us.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import Dimensions from 'react-dimensions';
 import  {Stage, Layer}  from 'react-konva';
 import { useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
 import classes from './SingleLineDiagram_us.module.scss';
 import Background from '../../components/SingleLineDiagram/backgroundGrid';
 import PanelArrayCollection from '../../components/SingleLineDiagram/PanelArrayCollection ';
@@ -12,20 +11,15 @@ import Disconnecter from '../../components/SingleLineDiagram/AcDisconnecter';
 import Meter from '../../components/SingleLineDiagram/Meter';
 import Grid from '../../components/SingleLineDiagram/Grid';
 import { ReactReduxContext, Provider, useSelector } from "react-redux";
-import { genReport, getReport } from '../../pages/Report/service'
-import { setReportData } from '../../store/action/reportAction'
 import ServerPanel from '../../components/SingleLineDiagram/ServicePanel';
-import { Table, Button, Tabs, Tooltip, notification } from 'antd';
+import { Table, Button, Tabs, Tooltip } from 'antd';
 import { ProfileOutlined } from '@ant-design/icons';
-import { saveProject } from '../../pages/Project/service'
-import { setBuildingReGenReport } from '../../store/action/index'
 import * as DataGenerator from '../../utils/singleLineDiagramDataGenerator';
 
 const { TabPane } = Tabs;
 
-const SingleLineDiagUS = (props) => {
-  const dispatch = useDispatch()
-  const { projectID, buildingID } = useParams();
+const SingleLineDiagUS = () => {
+  const { buildingID } = useParams();
 
   const [stageWidth,setStageWidth] = useState(window.innerWidth);
   const [stageHeight, setStageHeight] = useState(window.innerHeight);
@@ -76,11 +70,9 @@ const SingleLineDiagUS = (props) => {
   const tableTriggerHandler = () => {
     setTable(!tableTrigger);
   }
-  console.log(props.containerWidth)
-  console.log(props)
-  return (
 
-    <div>
+  return (
+    <div className={classes.SLD}>
       <Tooltip title="Equipment Table">
         <Button
           className={classes.triggerButton}
@@ -89,139 +81,145 @@ const SingleLineDiagUS = (props) => {
           icon={<ProfileOutlined />}
           onClick={tableTriggerHandler} />
       </Tooltip>
-      <div>
-        <Tabs
-          hidden={tableTrigger}
-          className={classes.tab}
-          tabBarStyle={{
-            backgroundColor: 'rgba(255,255,255, 0.5)',
-            paddingLeft: 30,
-            marginBottom: 0
-          }}
-        >
-          <TabPane tab="Module" key="1">
-            <Table
-              columns={[{
-                title: <div style={{fontSize: 10}} >Array</div>,
-                width: 60,
-                dataIndex: "array",
-                key: "array_module",
-                align: 'center',
-                ellipsis: true
-              },{
-                title: <div style={{fontSize: 10}} >Module Model</div>,
-                width: 150,
-                dataIndex: "module_model",
-                key: "module_model",
-                align: 'center',
-                ellipsis: true
-              },{
-                title: <div style={{fontSize: 10}} >DC Cable Length</div>,
-                width: 120,
-                dataIndex: "dc_cable_choice",
-                key: "cable_length",
-                align: 'center',
-                ellipsis: true
-              },{
-                title: <div style={{fontSize: 10}} >Module in Series</div>,
-                width: 120,
-                dataIndex: "module_in_series",
-                key: "module_in_series",
-                align: 'center',
-                ellipsis: true
-              },{
-                title: <div style={{fontSize: 10}} >Parallel String</div>,
-                width: 110,
-                dataIndex: "parallel_string",
-                key: "parallel_string",
-                align: 'center',
-                ellipsis: true
-              } ]}
-              dataSource={pvTableData}
-              pagination={false}
-              scroll={{ x: 100, y: 200 }}
-            />
-          </TabPane>
-          <TabPane tab="Inverter" key="2">
-          <Table columns={[{
-            title: <div style={{fontSize: 10}} >Array</div>,
+      <Tabs
+        hidden={tableTrigger}
+        className={classes.tab}
+        tabBarStyle={{
+          backgroundColor: 'rgba(255,255,255, 0.5)',
+          paddingLeft: 30,
+          marginBottom: 0
+        }}
+      >
+        <TabPane tab="Module" key="1">
+          <Table
+            columns={[{
+              title: <div style={{fontSize: 10}} >Array</div>,
+              width: 60,
+              dataIndex: "array",
+              key: "array_module",
+              align: 'center',
+              ellipsis: true
+            },{
+              title: <div style={{fontSize: 10}} >Module Model</div>,
+              width: 150,
+              dataIndex: "module_model",
+              key: "module_model",
+              align: 'center',
+              ellipsis: true
+            },{
+              title: <div style={{fontSize: 10}} >DC Cable Length</div>,
+              width: 120,
+              dataIndex: "dc_cable_choice",
+              key: "cable_length",
+              align: 'center',
+              ellipsis: true
+            },{
+              title: <div style={{fontSize: 10}} >Module in Series</div>,
+              width: 120,
+              dataIndex: "module_in_series",
+              key: "module_in_series",
+              align: 'center',
+              ellipsis: true
+            },{
+              title: <div style={{fontSize: 10}} >Parallel String</div>,
+              width: 110,
+              dataIndex: "parallel_string",
+              key: "parallel_string",
+              align: 'center',
+              ellipsis: true
+            } ]}
+            dataSource={pvTableData}
+            pagination={false}
+            scroll={{ x: 100, y: 200 }}
+          />
+        </TabPane>
+        <TabPane tab="Inverter" key="2">
+        <Table columns={[{
+          title: <div style={{fontSize: 10}} >Array</div>,
+          width: 150,
+          dataIndex: "array",
+          key: "array",
+          align: 'center',
+          ellipsis: true
+          },{
+            title: <div style={{fontSize: 10}} >Inverter Model</div>,
             width: 150,
-            dataIndex: "array",
-            key: "array",
+            dataIndex: "inverter_model",
+            key: "inverter_model",
             align: 'center',
             ellipsis: true
-            },{
-              title: <div style={{fontSize: 10}} >Inverter Model</div>,
-              width: 150,
-              dataIndex: "inverter_model",
-              key: "inverter_model",
-              align: 'center',
-              ellipsis: true
-            }, {
-              title: <div style={{fontSize: 10}} >AC Cable Length</div>,
-              width: 200,
-              dataIndex: "ac_cable_length",
-              key: "ac_cable_length",
-              align: 'center',
-              ellipsis: true
-            }]}
-            dataSource={invTableData}
-            scroll={{ x: 100, y: 200 }}
-            pagination={false}
-          />
-          </TabPane>
-        </Tabs>
-      </div>
+          }, {
+            title: <div style={{fontSize: 10}} >AC Cable Length</div>,
+            width: 200,
+            dataIndex: "ac_cable_length",
+            key: "ac_cable_length",
+            align: 'center',
+            ellipsis: true
+          }]}
+          dataSource={invTableData}
+          scroll={{ x: 100, y: 200 }}
+          pagination={false}
+        />
+        </TabPane>
+      </Tabs>
 
       <ReactReduxContext.Consumer>
         {({ store }) => (
           <Stage
             className={classes.stage}
-            height={props.containerWidth * 0.5625}
-            width={props.containerWidth + 30}>
+            height={stageHeight}
+            width={stageWidth}
+          >
             <Provider store={store}>
-              <Layer draggable={true}>
+              <Layer>
                 <Background
-                  width={props.containerWidth}
-                  height={props.containerWidth * 0.5625}/>
-                  <PanelArrayCollection
-                    width={props.containerWidth}
-                    height={stageHeight}
-                    numOfArray={numOfInverter}
-                    stringOfPanels={stringPanels}
-                    panelsOfInverter={panelsInverter}
-                    pvTable={pvTableData}/>
-                  <InverterCollection
-                    width={stageWidth}
-                    height={stageHeight}
-                    numOfInverter={numOfInverter}
-                    invertersData={invTableData}
-                    pvTable={pvTableData}/>
-                  <InterConnect
-                    width={stageWidth}
-                    height={stageHeight}
-                    numOfInverter={numOfInverter}
-                    combineBoxName={combiboxName}
-                    combiTable={combiboxCableChoice}/>
-                  <Disconnecter
-                    width={stageWidth}
-                    height={stageHeight}
-                    combiTable={combiboxCableChoice}
-                    numOfArray={numOfInverter}/>
-                  <ServerPanel
-                    width={stageWidth}
-                    height={stageHeight}
-                    combiTable={combiboxCableChoice}/>
-                  <Meter
-                    width={stageWidth}
-                    height={stageHeight}/>
-                  <Grid
-                    width={stageWidth}
-                    height={stageHeight}/>
+                  width={stageWidth}
+                  height={stageHeight}
+                />
+                <PanelArrayCollection
+                  width={stageWidth}
+                  height={stageHeight}
+                  numOfArray={numOfInverter}
+                  stringOfPanels={stringPanels}
+                  panelsOfInverter={panelsInverter}
+                  pvTable={pvTableData}
+                />
+                <InverterCollection
+                  width={stageWidth}
+                  height={stageHeight}
+                  numOfInverter={numOfInverter}
+                  invertersData={invTableData}
+                  pvTable={pvTableData}
+                />
+                <InterConnect
+                  width={stageWidth}
+                  height={stageHeight}
+                  numOfInverter={numOfInverter}
+                  combineBoxName={combiboxName}
+                  combiTable={combiboxCableChoice}
+                />
+                <Disconnecter
+                  width={stageWidth}
+                  height={stageHeight}
+                  combiTable={combiboxCableChoice}
+                  numOfArray={numOfInverter}
+                />
+                <ServerPanel
+                  width={stageWidth}
+                  height={stageHeight}
+                  combiTable={combiboxCableChoice}
+                />
+                <Meter
+                  width={stageWidth}
+                  height={stageHeight}
+                />
+                <Grid
+                  width={stageWidth}
+                  height={stageHeight}
+                />
               </Layer>
             </Provider>
           </Stage>
-
         )}
       </ReactReduxContext.Consumer>
     </div>
@@ -229,4 +227,4 @@ const SingleLineDiagUS = (props) => {
 }
 
 
-export default Dimensions()(SingleLineDiagUS);
+export default SingleLineDiagUS;
