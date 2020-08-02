791af3b4dd97db6d7cc1975f34f9081765ce1490
CMshadow <bjwmc1996@gmail.com>
2020-08-02 10:55:04 -0700

    finish sunPosition chart

--- src/components/ReportCharts/SunPositionChart.js
+++ src/components/ReportCharts/SunPositionChart.js
@@ -10,6 +10,7 @@ export const SunPositionChart = ({buildingID}) => {
   const { t } = useTranslation()
   const reportData = useSelector(state => state.report)
 
+  // 制造以月为单位的数据
   const monthlyData = Array(12).fill([]).map(ary => ([]))
   reportData[buildingID].sunPosition.forEach((monthData, monthIndex) =>
     monthData[0].forEach((sunAz, hourIndex) =>
@@ -17,48 +18,90 @@ export const SunPositionChart = ({buildingID}) => {
     )
   )
 
+  // 制造以小时为单位的数据
   const hourlyData = Array(monthlyData[0].length).fill([]).map(ary => ([]))
   reportData[buildingID].sunPosition.forEach((monthData, monthIndex) =>
     monthData[0].forEach((sunAz, hourIndex) =>
       (hourlyData[hourIndex].push([sunAz, monthData[1][hourIndex]]))
     )
   )
+  // 小时数据再补一次首月数据让数据闭环
+  hourlyData.forEach(hourData => hourData.push(hourData[0]))
 
+  // 图标配置项
   const option = {
+    tooltip: {
+      show: false,
+      trigger: 'axis',
+      axisPointer: {type: 'cross'}
+    },
+    axisPointer: {
+      link: {xAxisIndex: 'all'},
+      label: {backgroundColor: '#595959'}
+    },
     xAxis: {
       type: 'value',
-      name: 'Azimuth',
+      name: t('sunPosition.sunAzi'),
       nameLocation: 'center',
       nameTextStyle: titleStyle,
       nameGap: 25,
       min: value => value.min - 10,
       max: value => value.max + 10,
       splitNumber: 10,
-      splitLine: {show: false}
+      splitLine: {show: false},
+      axisLine: {show: false},
+      axisTick: {show: false},
+      axisLabel: {
+        showMinLabel: false,
+        showMaxLabel: false,
+        formatter: '{value}°'
+      },
+      axisPointer: {
+        show: true,
+        lineStyle: {
+          width: 2,
+          type: 'dashed',
+          color: '#bfbfbf'
+        }
+      }
     },
     yAxis: {
       type: 'value',
-      name: 'El',
+      name: t('sunPosition.sunElev'),
       nameLocation: 'center',
       nameTextStyle: titleStyle,
-      nameGap: 25,
+      nameGap: 35,
       min: 0,
       max: 90,
-      splitNumber: 10
+      splitNumber: 10,
+      axisLine: {show: false},
+      axisTick: {show: false},
+      axisLabel: {formatter: '{value}°'},
+      axisPointer: {
+        show: true,
+        lineStyle: {
+          width: 2,
+          type: 'dashed',
+          color: '#bfbfbf'
+        }
+      }
     },
     series: [
+      // 月单位数据
       ...monthlyData.map((monthData, monthIndex) => {
         const setup =  {
           data: monthData,
           smooth: true,
           type: 'line',
           lineStyle: {color: '#1890ff'},
+          symbol: 'none'
         }
+        // 仅这些月份有线上标识
         if ([12, 1, 2, 3, 4, 5, 6].includes(monthIndex + 1)) {
           const maxY = monthData.reduce((max, val) => val[1] > max ? val[1] : max, 0)
           const text = [12, 6].includes(monthIndex + 1) ?
-            t(`acPowerChart.month.${monthIndex + 1}`) :
-            `${t(`acPowerChart.month.${monthIndex + 1}`)} & ${t(`acPowerChart.month.${12 - (monthIndex + 1)}`)}`
+            `${t(`sunPosition.month.${monthIndex + 1}`)} ${t('sunPosition.day.21')}` :
+            `${t(`sunPosition.month.${monthIndex + 1}`)}/${t(`sunPosition.month.${12 - (monthIndex + 1)}`)} ${t('sunPosition.day.21')}`
           setup.markPoint = {
             data: [{
               symbol: 'rect',
@@ -69,13 +112,15 @@ export const SunPositionChart = ({buildingID}) => {
               label: {
                 position: [12, 4, 5, 6].includes(monthIndex + 1) ? 'bottom' : 'top',
                 fontSize: 14,
-                color: '#000',
+                color: '#5b8c00',
+                backgroundColor: 'rgba(255, 255, 255, 0.3)'
               }
             }]
           }
         }
         return setup
       }),
+      // 小时单位数据
       ...hourlyData.map((hourData, hourIndex) => {
         const allZero = hourData.every(pair => pair[1] === 0)
         const maxY = hourData.reduce((max, val) => val[1] > max ? val[1] : max, 0)
@@ -85,7 +130,13 @@ export const SunPositionChart = ({buildingID}) => {
           smooth: true,
           type: 'line',
           lineStyle: {color: '#fa8c16', width: 3},
+          symbolSize: 5,
+          itemStyle: {color: '#fa8c16'}
         }
+        // 不同月份标识位置不同
+        const position = [0, 1, 2, 3, 4, 5, 6].includes(hourIndex) ? 'left' :
+          [10, 11, 12, 13, 14, 15, 16].includes(hourIndex) ? 'right' : 'top'
+        // 数据不全为0时渲染标识
         if (!allZero) {
           setup.markPoint = {
             data: [{
@@ -95,9 +146,10 @@ export const SunPositionChart = ({buildingID}) => {
               value: t(`sunPosition.hour.${hourIndex}`),
               name: t(`sunPosition.hour.${hourIndex}`),
               label: {
-                position: 'top',
+                position: position,
+                offset: [0, -10],
                 fontSize: 14,
-                color: '#5b8c00',
+                color: '#c41d7f',
               }
             }]
           }
@@ -111,7 +163,7 @@ export const SunPositionChart = ({buildingID}) => {
     <Card
       title={
         <Title style={{textAlign: 'center'}} level={4}>
-          {t('heatMap.title')}
+          {t('sunPosition.title')}
         </Title>
       }
       hoverable
