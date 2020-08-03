5de50238f14c96f59ccf2a30bdf362626246e2c4
CMshadow <bjwmc1996@gmail.com>
2020-08-02 18:06:14 -0700

    handle existing reports & update version

--- src/pages/Report/tabs/ProdTab.js
+++ src/pages/Report/tabs/ProdTab.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, Row, Col } from 'antd'
+import { useSelector } from 'react-redux'
 import { ACPowerTable } from '../../../components/ACPowerTable/ACPowerTable'
 import { ACPowerChart } from '../../../components/ReportCharts/ACPowerChart'
 import { ProductionChart } from '../../../components/ReportCharts/ProductionChart'
@@ -7,6 +8,8 @@ import { IrrVSProdChart } from '../../../components/ReportCharts/IrrVSProdChart'
 import { ACDistributionChart } from '../../../components/ReportCharts/ACDistributionChart'
 
 export const ProdTab = ({buildingID}) => {
+  const reportData = useSelector(state => state.report)
+
   return (
     <Card bordered={false}>
       <Row gutter={[0, 25]}>
@@ -24,16 +27,26 @@ export const ProdTab = ({buildingID}) => {
           <ProductionChart buildingID={buildingID}/>
         </Col>
       </Row>
-      <Row gutter={[0, 25]}>
-        <Col span={24}>
-          <IrrVSProdChart buildingID={buildingID}/>
-        </Col>
-      </Row>
-      <Row gutter={[0, 25]}>
-        <Col span={24}>
-          <ACDistributionChart buildingID={buildingID}/>
-        </Col>
-      </Row>
+      {/* New Chart Added at Beta 0.3.0 */}
+      {
+        reportData[buildingID].daily_AC_power ?
+        <Row gutter={[0, 25]}>
+          <Col span={24}>
+            <IrrVSProdChart buildingID={buildingID}/>
+          </Col>
+        </Row> :
+        null
+      }
+      {/* New Chart Added at Beta 0.3.0 */}
+      {
+        reportData[buildingID].ac_output_distribution ?
+        <Row gutter={[0, 25]}>
+          <Col span={24}>
+            <ACDistributionChart buildingID={buildingID}/>
+          </Col>
+        </Row> :
+        null
+      }
     </Card>
   )
 }
