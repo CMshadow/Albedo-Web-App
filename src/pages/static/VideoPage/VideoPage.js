4ca7e67b414424d896ec4bd5565672a55aa059a6
rousong-w <68358144+rousong-w@users.noreply.github.com>
2020-08-04 02:23:06 -0700

    update videopage

--- src/pages/static/VideoPage/VideoPage.js
+++ src/pages/static/VideoPage/VideoPage.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import DisplayPageHeader from '../../../layouts/DisplayPageHeader/DisplayPageHeader';
 import DisplayPageFooter from '../../../layouts/DisplayPageFooter/DisplayPageFooter';
-//import getUrl from './getUrl';
+
 import { Layout ,Typography} from 'antd';
 import Player from 'aliplayer-react';
 import * as styles from './VideoPage.module.scss';
-
+//import * as A  from './getUrl';
 const { Content } = Layout;
 const { Title } = Typography;
-
-
 const config = {
-    source:' https://outin-24065ccdd13511ea962100163e1c60dc.oss-cn-shanghai.aliyuncs.com/sv/8f593b9-173ad9b1d30/8f593b9-173ad9b1d30.mp4?Expires=1596352747&OSSAccessKeyId=LTAIxSaOfEzCnBOj&Signature=9MU7%2BnRU9MjIwLjwN7lDiGbqt8g%3D',  
+    source:'https://albedo-tutorial.oss-cn-shanghai.aliyuncs.com/Albedo%E4%BD%BF%E7%94%A8%E6%95%99%E7%A8%8B.mp4',  
     width: "100%",
     height: "500px",
     autoplay: true,
@@ -20,24 +18,27 @@ const config = {
     playsinline: true,
     preload: true,
     controlBarVisibility: "hover",
-    useH5Prism: true,
-   
-  };
-const VideoPage = ()=>(
+    useH5Prism: true
+};
+
+
+const VideoPage = () => {
+    
+    return(
     <div >
-    <Layout style={{backgroundColor:'#ffffff'}}>
-      <DisplayPageHeader/>
-     
-      <Content className = {styles.content}>
-        <Title className={styles.title1}>Albedo 快速上手教程</Title>
-      
-        <Title level={4} type="secondary" className={styles.title2}>三分钟快速上手，带你玩转光伏设计</Title>
-        <br/>
-        <Player config={config} />
-      </Content>
-      <DisplayPageFooter/>
-    </Layout>
+        <Layout style={{backgroundColor:'#ffffff'}}>
+          <DisplayPageHeader/>
+          <Content className = {styles.content}>
+            <Title className={styles.title1}>Albedo 快速上手教程</Title>         
+            <Title level={4} type="secondary" className={styles.title2}>三分钟快速上手，带你玩转光伏设计</Title>
+            <br/>
+            <Player config={config} />
+
+          </Content>
+          <DisplayPageFooter/>
+        </Layout>
     </div>
-);
+    )
+};
 
 export default VideoPage;
--- src/pages/static/VideoPage/getUrl.js
+++ src/pages/static/VideoPage/getUrl.js
@@ -11,23 +11,49 @@ function initVodClient(accessKeyId, secretAccessKey,) {
 
     return client;
 }
-var client = initVodClient('LTAI4G8qrB7zzPDKKLRKMDFs','f6myZK4ZvmtAHTlEhZVwl9vvfkINGK');
-client.request("GetPlayInfo", {
-    VideoId: '0cbc5384e4dd4a5d9cbadb3b2c550d3c'
-}, {}).then(function (response) {
-    // play url
-    if (response.PlayInfoList && response.PlayInfoList.PlayInfo && response.PlayInfoList.PlayInfo.length > 0){
-        for (var i=0; i<response.PlayInfoList.PlayInfo.length; i++){
-            console.log("PlayInfo.PlayURL = " + response.PlayInfoList.PlayInfo[i].PlayURL);
-        }
-    }
-    // base metadata
-    if (response.VideoBase){
-        console.log('VideoBase.Title = ' + response.VideoBase.Title);
-    }
-    console.log('RequestId = ' + response.RequestId);
-}).catch(function (response) {
+export const getUrl = () => {
+    const client = initVodClient('LTAI4G8qrB7zzPDKKLRKMDFs','f6myZK4ZvmtAHTlEhZVwl9vvfkINGK');
     
-    console.log('ErrorMessage = ' + response.data.Message);
-    console.log('RequestId = ' + response.data.RequestId);
-});
+    client.request("GetPlayInfo", {
+            VideoId: '0cbc5384e4dd4a5d9cbadb3b2c550d3c'
+        }, {}).then(function (response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+            // play url
+            if (response.PlayInfoList && response.PlayInfoList.PlayInfo && response.PlayInfoList.PlayInfo.length > 0){
+                for (var i=0; i<response.PlayInfoList.PlayInfo.length; i++){
+                    //console.log("PlayInfo.PlayURL = " + response.PlayInfoList.PlayInfo[i].PlayURL); 
+                    return(
+                        {
+                            source: response.PlayInfoList.PlayInfo[i].PlayURL,  
+                            width: "100%",
+                            height: "500px",
+                            autoplay: true,
+                            isLive: false,
+                            rePlay: false,
+                            playsinline: true,
+                            preload: true,
+                            controlBarVisibility: "hover",
+                            useH5Prism: true
+                        }
+                    )  
+                }
+            }
+            //base metadata
+            if (response.VideoBase){
+                console.log('VideoBase.Title = ' + response.VideoBase.Title);
+            }
+            console.log('RequestId = ' + response.RequestId);
+        }).catch(function (response) {
+            console.log('ErrorCode = ' + response.data.Code);
+            console.log('ErrorMessage = ' + response.data.Message);
+            console.log('RequestId = ' + response.data.RequestId);
+        })
+    // return(
+        
+    //     config
+    // );
+
+}
